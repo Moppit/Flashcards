2e4851797788c49823056c53a8b48ede4a251b27
Moppit <michelletranusa@gmail.com>
2020-03-12 23:01:42 -0600

    Have multiple card functionality

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
+// Flashcard struct
+struct Flashcard {
+    var question: String
+    var answer: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var question_label: UILabel!
     @IBOutlet weak var answer_label: UILabel!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     var iterator = 0
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateFlashcard(question: "What color is the typical female budgie's cere?", answer: "Pink or reddish brown")
     }
     
     // Need to figure out what this does and how it does it
@@ -40,9 +52,52 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String) {
-        question_label.text = question
-        answer_label.text = answer
+        let flashcard = Flashcard(question: question, answer: answer)
+        flashcards.append(flashcard)
+        
+        currentIndex = flashcards.count - 1
+        print("Added new flashcard! Current index: \(currentIndex)")
+        print("We now have \(flashcards.count) flashcards")
+        
+        updateNextPrevButtons()
+        updateLabels()
     }
+    
+    func updateLabels(){
+        let currentFlashcard = flashcards[currentIndex]
+        question_label.text = currentFlashcard.question
+        answer_label.text = currentFlashcard.answer
+    }
+    
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    func updateNextPrevButtons() {
+            print("Update Next Called")
+            // Toggle enable/disable for next button
+            if (currentIndex == flashcards.count - 1) {
+                nextButton.isEnabled = false
+            }
+            else {
+                nextButton.isEnabled = true
+            }
+            // Toggle enable/disable for prev button
+            if (currentIndex == 0) {
+                prevButton.isEnabled = false
+            }
+            else {
+                prevButton.isEnabled = true
+            }
+        }
     
 }
 
